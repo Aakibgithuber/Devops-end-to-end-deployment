7df3ae33de07a842e4fdb72b487fd5eed176cb53
Ubuntu <ubuntu@ip-172-31-36-185.ap-south-1.compute.internal>
2020-10-12 10:50:08 +0000

    create a ecr repo

---
+++ create_aws_ecr_repo.sh
@@ -0,0 +1,23 @@
+# RUN me where kubectl is available,& make sure to replace account,region etc
+#
+ACCOUNT=403959134869
+REGION=ap-south-1
+SECRET_NAME=${REGION}-ecr-registry
+EMAIL=ybmadhu404@gmail.com
+
+#
+# Fetch token (which will expire in 12 hours)
+#
+
+TOKEN=`aws ecr --region=$REGION get-authorization-token --output text --query authorizationData[].authorizationToken | base64 -d | cut -d: -f2`
+
+#
+# Create or replace registry secret
+#
+
+kubectl delete secret --ignore-not-found $SECRET_NAME
+kubectl create secret docker-registry $SECRET_NAME \
+ --docker-server=https://${ACCOUNT}.dkr.ecr.${REGION}.amazonaws.com \
+ --docker-username=AWS \
+ --docker-password="${TOKEN}" \
+ --docker-email="${EMAIL}"
