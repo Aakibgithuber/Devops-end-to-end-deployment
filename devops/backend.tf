58c38c1beaa5c5e48ce46a3ecebb0cfc30fd6cd7
EC2 Default User <ec2-user@ip-172-31-43-90.ap-south-1.compute.internal>
2021-02-19 19:03:22 +0000

    remove bakend

--- devops/backend.tf
+++ devops/backend.tf
@@ -1,11 +1,11 @@
 
-terraform {
-  backend "s3" {
-    bucket = "jms-terraform-backend"
-    key    = "jmsth_jenkins.tfstate"
-    region = "ap-south-1"
-    encrypt = true
-    dynamodb_table = "terraform-state-lock-dynamo"
-    }
-  }
+#terraform {
+#  backend "s3" {
+#    bucket = "jms-terraform-backend"
+#    key    = "jmsth_jenkins.tfstate"
+#    region = "ap-south-1"
+#    encrypt = true
+#    dynamodb_table = "terraform-state-lock-dynamo"
+#    }
+#  }
 
